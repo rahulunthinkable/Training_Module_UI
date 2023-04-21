84b606b79c8a5913c320f1769c47e14e89ddc069
prafullcodes <prafull.mangla@daffodilsw.com>
2023-04-21 18:19:51 +0530

    forkJoin added

--- src/app/service/course-service/course-service.service.ts
+++ src/app/service/course-service/course-service.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { GenericHttpService } from '../generic-http.service';
 import { courseUrl, url } from 'src/app/utils/urls';
-import {Observable, forkJoin} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +9,10 @@ export class CourseService {
 
   constructor(private genericHttpService:GenericHttpService) { }
 
-  public requestFromMulultibleResource(): Observable<any[]>{
-    let request1= this.genericHttpService.httpGet(courseUrl.COURSE_URL)
-    let request2 =this.genericHttpService.httpGet(courseUrl.CATEGORY_URL)
-
-    return forkJoin([request1,request2])
+  getAllCourse(){
+    return this.genericHttpService.httpGet(courseUrl.COURSE_URL)
   }
-
+  getAllCategories(){
+    return this.genericHttpService.httpGet(courseUrl.CATEGORY_URL)
+  }
 }
--- src/app/shared/components/course/course.component.ts
+++ src/app/shared/components/course/course.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from "@angular/core";
 import { CourseService } from "src/app/service/course-service/course-service.service";
 import { SpinnerService } from "src/app/service/spinner/spinner.service";
 import { courseFilter } from "./course-filter/course-filter";
+import { catchError, forkJoin, of, take } from "rxjs";
+import { ErrorMessages } from "src/app/utils/error-messages";
+import { SnackService } from "src/app/service/snack-bar/snack.service";
+import { SnackClasses } from "src/app/utils/snack-bar-classes";
 
 @Component({
   selector: "app-course",
@@ -14,30 +18,43 @@ export class CourseComponent implements OnInit {
   filterOptions: any = courseFilter;
   constructor(
     private courseApiService: CourseService,
-    private categoryApiService: CourseService,
-    private spinnerService: SpinnerService
+    private spinnerService: SpinnerService,
+    private snackBarService: SnackService
   ) {}
 
   ngOnInit(): void {
-    this.spinnerService.loadSpinner();
-    this.courseApiService
-      .requestFromMulultibleResource()
-      .subscribe((responseList: any[]) => {
+    const req1 = this.courseApiService.getAllCourse().pipe(take(1),catchError((err)=>{
+      return of(null)
+    }))
+    let req2 = this.courseApiService.getAllCategories().pipe(take(1),catchError((err)=>{
+      return of(null)
+    }))
+
+    this.spinnerService.loadSpinner()
+
+    forkJoin([req1, req2]).subscribe({
+      next: (responseList: any) => {
         this.courseData = responseList[0];
+        this.spinnerService.closeSpinner();
         this.courseData = this.courseData.map((course: any) => {
           course.image = `data:image/png;base64,${course.image}`;
           return course;
         });
-
         this.categories = responseList[1];
         this.filterOptions[0].options = this.categories.map((category: any) => {
           category["viewValue"] = category.categoryName;
           return category;
         });
-        this.spinnerService.closeSpinner();
-      });
+      },
+      error: (error) => {
+        this.snackBarService.openSnackBar(
+          ErrorMessages.SOMETHING_WENT_WRONG,
+          100,
+          SnackClasses.ERROR
+        );
+      },
+    });
   }
 
-  selectedFilters(event: Event) {
-  }
+  selectedFilters(event: Event) {}
 }
